0f6f31ccfe7eb1c3ff39c7fe4acebc9765129d9a
root <root@lizard.Lizard>
2020-05-09 06:03:13 +0200

    Adding application java source

---
+++ src/test/java/clinic/programming/training/App.java
@@ -0,0 +1,14 @@
+
+package com.journaldev.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
